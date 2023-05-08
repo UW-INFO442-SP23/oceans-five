6052e8f7751e2cd549ee08c6e014606869fe5160
brendanwilliam <brendanwillkeane@gmail.com>
2023-05-08 13:32:41 -0700

    All Hawaii beach data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
