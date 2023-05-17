6338f307d0384c397a205d75c16d4f33eacbd79b
Evie Goodman <evelyng@uoregon.edu>
2023-05-17 16:12:24 -0700

    favicon fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Nav(props) {
     return (
         <div class='navbar'>
-            <div class='nav-buttons'><a href="/Search"><p>Locate</p></a></div>
+            <div class='nav-buttons' style={{marginLeft: 45}}><a href="/Search"><p>Locate</p></a></div>
             <div class='nav-buttons'><a href="/Learn"><p>Learn</p></a></div>
             <div class='nav-buttons'><a href="/Donate"><p>Donate</p></a></div>
-        </div>
+            </div>
     );
 }
