fb1b7378dfccf126f7dd57353b7a28bbcbcfbf65
Evie Goodman <evelyng@uoregon.edu>
2023-05-18 15:06:43 -0700

    few edits

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -4,18 +4,7 @@ import Nav from './Nav';
 
 export default function Donate(props) {
     return (
-        <div class='landing'>
-            <div class="landing-text">
-                <div><h1>Discover life underwater through snorkeling</h1></div>
-                <div>
-                    <Link to='/Search'>
-                        <button type="button" id="dive-in">
-                            <h4>Dive in!</h4>
-                        </button>
-                    </Link>
-                </div>
-                <div><p>New to snorkeling? Learn more.</p></div>
-            </div>
+        <div>
         </div>
     );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Nav(props) {
     return (
         <div class='navbar'>
-            <div class='nav-buttons' style={{marginLeft: 45}}><a href="/Search"><p>Locate</p></a></div>
+            <div class='nav-buttons' style={{marginLeft: 45}}><a href="/"><p>Home</p></a></div>
+            <div class='nav-buttons'><a href="/Search"><p>Locate</p></a></div>
             <div class='nav-buttons'><a href="/Learn"><p>Learn</p></a></div>
             <div class='nav-buttons'><a href="/Donate"><p>Donate</p></a></div>
             </div>
