b43ca13e2c269334a6800da9d81a64acd9feeef0
Evie Goodman <evelyng@uoregon.edu>
2023-05-24 12:37:37 -0700

    learn and donate

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -5,6 +5,10 @@ import Nav from './Nav';
 export default function Donate(props) {
     return (
         <div>
+            <div id="static-pages-text">
+                <h1>Donate to help wildlife conservation</h1>
+                <h4>Whether it's your time, money, or knowledge, all efforts towards conservation count.</h4>
+            </div>
         </div>
     );
 }
