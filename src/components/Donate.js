26a4c50349b50db77b23baf7bb13065fc902ba8a
Owen Schaff <oschaff@uw.edu>
2023-05-31 10:41:11 -0700

    clearing bugs

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Nav from './Nav';
 import donate from '../icons/donatebackground.png';
 export default function Donate(props) {
     return (
